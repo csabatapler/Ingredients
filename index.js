8a1a80af494ba1f6acecd2b5fc4b6a3894356cb5
Csaba Tapler <164663981+csabatapler@users.noreply.github.com>
2024-05-08 15:13:59 +0200

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-
 const appSettings = {
     databaseURL: "https://shoppinglist-b0f7d-default-rtdb.europe-west1.firebasedatabase.app"
 }
-import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const app =initializeApp(appSettings)
 const database = getDatabase(app)
@@ -17,24 +17,49 @@ addBtn.addEventListener("click", function() {
     let inputValue = inputEl.value
     if (inputValue) { 
     push(shoppingListInDB, inputValue)
-    appendToList(inputValue)
     clearInputFieldEl()    
     }
 })
-function appendToList(item) {
-    shoppingListEl.innerHTML += `<li>${item}</li>`
+
+onValue(shoppingListInDB, function(snapshot) {
+    
+        
+
+    if (snapshot.exists()) {
+        console.log("exists")
+        let shoppingListArray = Object.entries(snapshot.val())
+        clearShoppingListEl()
+        for (let i=0; i<shoppingListArray.length; i++) {
+            let currentItem = shoppingListArray[i]
+            let currentItemKey = currentItem[0]
+            let currentItemValue = currentItem[1]
+            console.log(currentItemValue, currentItemKey)
+            appendToList(currentItem)
+        }
+    } else {
+    console.log("does not exist")
+    shoppingListEl.innerHTML = "No items in list"
+}
+})
+
+function appendToList(item){
+    //shoppingListEl.innerHTML += `<li>${item}</li>`
+    let newEl = document.createElement("li")
+    let itemID = item[0]
+    let itemValue = item[1]
+    newEl.textContent = itemValue
+    shoppingListEl.append(newEl)
+    newEl.tabindex = 0
+    newEl.addEventListener("dblclick", function() {
+        let exactLocationInDB = ref(database, `shoppingList/${itemID}`)
+        remove(exactLocationInDB)
+    })
+}
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
 }
 
 function clearInputFieldEl() {
     inputEl.value = ""
 }
-
-onValue(shoppingListInDB, function(snapshot) {
-    let shoppingListArray = Object.values(snapshot.val())
-    for (let i=0; i<shoppingListArray.length; i++) {
-        let currentItem = shoppingListArray[i]
-        console.log(currentItem)
-        appendToList(currentItem)
-    }
-    
-})
